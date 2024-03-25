5d83338e2b624df5055ed0d7718c7566ea7d929b
Adam Bertram <adbertram@gmail.com>
2024-03-25 09:04:59 -0500

    added appveyor build scripts

---
+++ buildscripts/publish.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = 'Stop'
+
+try {
+    ## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
+    ## Remove all of the files/folders to exclude out of the main folder
+    $excludeFromPublish = @(
+        'Bricklink\\buildscripts'
+        'Bricklink\\appveyor\.yml'
+        'Bricklink\\\.git'
+        'Bricklink\\\.nuspec'
+        'Bricklink\\README\.md'
+    )
+    $exclude = $excludeFromPublish -join '|'
+    Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | Where-Object { $_.FullName -match $exclude } | Remove-Item -Force -Recurse
+
+    ## Publish module to PowerShell Gallery
+    $publishParams = @{
+        Path        = $env:APPVEYOR_BUILD_FOLDER
+        NuGetApiKey = $env:nuget_apikey
+        Repository  = 'PSGallery'
+        Force       = $true
+        Confirm     = $false
+    }
+    Publish-Module @publishParams
+
+} catch {
+    Write-Error -Message $_.Exception.Message
+    $host.SetShouldExit($LastExitCode)
+}
