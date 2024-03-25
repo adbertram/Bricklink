234fdc206eda31e639807aff19d5551f0f25e8d4
Adam Bertram <adbertram@gmail.com>
2024-03-25 08:44:54 -0500

    improved findwantedlistitems function

--- private/FindWantedListItems.ps1
+++ private/FindWantedListItems.ps1
@@ -9,12 +9,13 @@ function FindWantedListItems {
 
     $ErrorActionPreference = 'Stop'
 
-    $json = $Html | Select-String -Pattern 'var wlJson = (\{.*\})' | ForEach-Object { $_.matches[0].groups[1].value }
-    $wiResponse = $json | ConvertFrom-Json
-    if (TestObjectProperty -InputObject $wiResponse -PropertyName 'wantedItems') {
-        if ($wiResponse.wantedItems.count -ne $wiResponse.totalResults) {
-            throw "Did not extract 100% of wanted list items in wanted list. This can sometimes happen if you're not logged in. Run Connect-Web and try again."
+    if ($json = $Html | Select-String -Pattern 'var wlJson = (\{.*\})' | ForEach-Object { $_.matches[0].groups[1].value }) {
+        $wiResponse = $json | ConvertFrom-Json
+        if (TestObjectProperty -InputObject $wiResponse -PropertyName 'wantedItems') {
+            if ($wiResponse.wantedItems.count -ne $wiResponse.totalResults) {
+                throw "Did not extract 100% of wanted list items in wanted list. This can sometimes happen if you're not logged in. Run Connect-Web and try again."
+            }
+            $wiResponse.wantedItems
         }
-        $wiResponse.wantedItems
     }
 }
