89618f423cfb251da4056f331e6b819bfefa86a9
Adam Bertram <adbertram@gmail.com>
2024-06-24 14:49:21 -0500

    revert

--- private/IncrementBricklinkApiCallCount.ps1
+++ private/IncrementBricklinkApiCallCount.ps1
@@ -1,6 +1,5 @@
 function IncrementBricklinkApiCallCount {
     $apiCallData = Get-BlApiCallCount
     $apiCallData.count++
-    ## not using JSON here becuause of an issue when multhreading
-    (Get-Content -Path $script:apiCallCountTrackingFilePath -Raw).replace('count": \d+','count": ' + $apiCallData.count) | Set-Content -Path $script:apiCallCountTrackingFilePath
+    $apiCallData | ConvertTo-Json | Set-Content -Path $script:apiCallCountTrackingFilePath
 }
